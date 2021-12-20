0f9bdd8e6c688707349972d6dd582f0c96747bc2
shiyuq <1543082564@qq.com>
2021-12-20 14:14:24 +0800

    tinymce 问题修复

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -27,7 +27,7 @@ import load from './dynamicLoadScript'
 
 // why use this cdn, detail see https://github.com/PanJiaChen/tinymce-all-in-one
 const tinymceCDN =
-  'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.10.2/tinymce.min.js'
 
 export default {
   name: 'Tinymce',
